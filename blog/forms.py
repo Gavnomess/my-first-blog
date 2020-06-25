120499951b895cc9223c3e69abed258b0a89f12c
Zamiralov_Viktor <zamiralov3ddd@mail.ru>
2020-06-25 18:04:34 +0300

    form and view

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class sum_form(forms.Form):
+     edit1 = forms.CharField()
+     edit2 = forms.CharField()
+     edit_result = forms.CharField()
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, sum_form, NameForm
+from django.http import HttpResponseRedirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by
@@ -38,3 +39,36 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def get_result(request):
+    if request.method == "POST":
+        
+        answer = sum_form(request.POST)
+
+        if answer.is_valid():
+            answer.edit_result = answer.cleaned_data['edit1'] + answer.cleaned_data['edit2']
+#            answer.save()
+#            return HttpResponseRedirect('')
+         
+#        answer.initial['edit_result'] = '5' 
+    else:
+        answer = sum_form()
+    return render(request, 'blog/test.html',{'answer': answer})
+		
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'blog/post_list.html', {'form': form})
