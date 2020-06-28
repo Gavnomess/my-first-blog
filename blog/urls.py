f0aa5a93a3534bcbdf1e55bbd890c844fc86bcd0
Zamiralov_Viktor <zamiralov3ddd@mail.ru>
2020-06-29 01:40:21 +0300

    GET request on REST

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 
-    path('test/', views.Result_view.as_view()),
-#    path('test/', views.Result_view, name='Result_view'),
+    path('test_full_Django/', views.get_result, name='get_result'),
+    path('test_REST_Django/', views.Result_view.as_view()),
+#    path('test/', views.Result_view, name='views.Result_view'),
 
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import Result_Serializer
 from rest_framework import generics
+from rest_framework.renderers import TemplateHTMLRenderer
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by
@@ -44,39 +45,71 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-class Result_view(generics.ListCreateAPIView):
-#class Result_view(APIView):
 
+############### Django REST framework ################
+
+#class Result_view(generics.ListCreateAPIView):
+#    queryset = summator_model.objects.all()
+#    serializer_class = Result_Serializer
+class Result_view(APIView):
+#class Result_view(generics.RetrieveAPIView):
 #    def get_html(request):
 #        if request.method == "GET":
 #            return render(request, 'blog/test.html',{'edits': edits})
-       
+    
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'blog/test_REST_Django.html'
 
-#    def get(self, request):
-#    edits = summator_model.objects.all()
-#    serializer = Result_Serializer(edits, many=True)
-#        return Response({"edits": serializer.data})
-    queryset = summator_model.objects.all()
-    serializer_class = Result_Serializer
-
-
-
-
-     # def get_result(request):
-     #   if request.method == "POST":       
-     #       answer = sum_form(request.POST)
-     #       answer1 = sum_form()
-
-     #   answer1.fields['edit_result'].label = ''
+    def get(self, request):
+        #queryset = summator_model.objects.all()
+        #serializer = Result_Serializer(edits, many=True)
+        #return Response({"edits": serializer.data})
+        #edits = get_object_or_404(summator_model)
+        edits = summator_model()
+        serializer = Result_Serializer(edits)
+        #return Response({'serializer': serializer, 'edits': edits})
         
-         
+       
+        return Response({'serializer': serializer})
 
-    #        a  = int (answer['edit1'].data) + int (answer['edit2'].data)
-    #        answer1.fields['edit_result'].initial = a
+    def post(self, request):
+        edits = summator_model(request.POST)
+        #a  = int (edits['edit1_model'].data) + int (edits['edit2_model'].data)
+        #edits.fields['edit_result_model'].initial = a
+       
+        serializer = Result_Serializer(edits)
+        
+        return Response({"serializer": serializer})
 
-   #         return render(request, 'blog/test.html',{'answer1': answer1})
+
+
+#    def post(self, request):
+#        edits = get_object_or_404(summator_model)
+#        serializer = Result_Serializer(edits, data=request.data)
+#        if not serializer.is_valid():
+#            return Response({'serializer': serializer, 'edits': edits})
+#        serializer.save()
+#        return redirect('edits-list')
+
+
+
+
+
+########### full Django solution ###############
+
+def get_result(request):
+    if request.method == "POST":       
+        answer = sum_form(request.POST)
+        answer1 = sum_form()
+
+        answer1.fields['edit_result'].label = ''
+        
+        a  = int (answer['edit1'].data) + int (answer['edit2'].data)
+        answer1.fields['edit_result'].initial = a
+
+        return render(request, 'blog/test_full_Django.html',{'answer1': answer1})
            
-   #     else:
-   #         answer = sum_form()
-   #     return render(request, 'blog/test.html',{'answer': answer})
+    else:
+        answer = sum_form()
+        return render(request, 'blog/test_full_Django.html',{'answer': answer})
 		
